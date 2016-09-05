73d0ee65e3a38be07f7cf8a5fcc95b5df9dfc01f
Nevyn <mark@thepcsite.co.uk>
2016-09-05 10:24:59 +0100

    Removed MQTT
    Removed the Adafruti MQTT librari as connectivity to adafruit.io seems to be problematic.  Return to this later.
    Tidied up some code - mostly cosmetic.

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -36,8 +36,6 @@
 #include <NtpClientLib.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
 #include <SparkFunTSL2561.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -90,52 +88,7 @@ DS3231 *rtc;
 #define PHANT_DOMAIN        "data.sparkfun.com"
 #define PHANT_PAGE          "/input/zDA9M8dQlahOqo4bx5Dd"
 #define PHANT_PORT          80
-
-//
-//  Stuff required for Adafruit IO.
-//
 WiFiClient _wifiClient;
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-
-// Store the MQTT server, client ID, username, and password in flash memory.
-// This is required for using the Adafruit MQTT library.
-const char MQTT_SERVER[] PROGMEM = AIO_SERVER;
-// Set a unique MQTT client ID using the AIO key + the date and time the sketch
-// was compiled (so this should be unique across multiple devices for a user,
-// alternatively you can manually set this to a GUID or other random value).
-const char MQTT_CLIENTID[] PROGMEM = AIO_KEY __DATE__ __TIME__;
-const char MQTT_USERNAME[] PROGMEM = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM = AIO_KEY;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&_wifiClient, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
-const char WINDSPEED_FEED[] PROGMEM = AIO_USERNAME "/feeds/windspeed";
-Adafruit_MQTT_Publish _windSpeedFeed = Adafruit_MQTT_Publish(&mqtt, WINDSPEED_FEED);
-
-const char WIND_DIRECTION[] PROGMEM = AIO_USERNAME "/feeds/winddirection";
-Adafruit_MQTT_Publish _windDirectionFeed = Adafruit_MQTT_Publish(&mqtt, WIND_DIRECTION);
-
-const char UVLIGHT_FEED[] PROGMEM = AIO_USERNAME "/feeds/uvlight";
-Adafruit_MQTT_Publish _uvLightFeed = Adafruit_MQTT_Publish(&mqtt, UVLIGHT_FEED);
-
-const char RAINFALL_FEED[] PROGMEM = AIO_USERNAME "/feeds/rainfall";
-Adafruit_MQTT_Publish _rainFallFeed = Adafruit_MQTT_Publish(&mqtt, RAINFALL_FEED);
-
-const char HUMIDITY_FEED[] PROGMEM = AIO_USERNAME "/feeds/humidity";
-Adafruit_MQTT_Publish _humidityFeed = Adafruit_MQTT_Publish(&mqtt, HUMIDITY_FEED);
-
-const char GROUND_TEMPERATURE_FEED[] PROGMEM = AIO_USERNAME "/feeds/groundTemperature";
-Adafruit_MQTT_Publish _groundTemperatureFeed = Adafruit_MQTT_Publish(&mqtt, GROUND_TEMPERATURE_FEED);
-
-const char DAYLIGHT_FEED[] PROGMEM = AIO_USERNAME "/feeds/daylight";
-Adafruit_MQTT_Publish _daylightFeed = Adafruit_MQTT_Publish(&mqtt, DAYLIGHT_FEED);
-
-const char AIR_TEMPERATURE_FEED[] PROGMEM = AIO_USERNAME "/feeds/airtemperature";
-Adafruit_MQTT_Publish _airTemperatureFeed = Adafruit_MQTT_Publish(&mqtt, AIR_TEMPERATURE_FEED);
-
-const char AIR_PRESSURE_FEED[] PROGMEM = AIO_USERNAME "/feeds/airpressure";
-Adafruit_MQTT_Publish _airPressureFeed = Adafruit_MQTT_Publish(&mqtt, AIR_PRESSURE_FEED);
 
 //
 //  Used for debugging, determine the output state of the onboard LED.
@@ -193,71 +146,6 @@ void PostDataToPhant()
         Debugger::DebugMessage("Error sending data to Phant.");
     }
     http.end();
-}
-
-//
-//  Function to connect and reconnect as necessary to the MQTT server.
-//  Should be called in the loop function and it will take care if connecting.
-//
-void MQTT_connect()
-{
-    int8_t result;
-
-    if (mqtt.connected())
-    {
-        return;
-    }
-
-    Debugger::DebugMessage("Connecting to MQTT... ");
-    while ((result = mqtt.connect()) != 0)
-    {
-        switch (result)
-        {
-            case 1: 
-                Debugger::DebugMessage("Wrong protocol");
-                break;
-            case 2: 
-                Debugger::DebugMessage("ID rejected");
-                break;
-            case 3: 
-                Debugger::DebugMessage("Server unavailable");
-                break;
-            case 4:
-                Debugger::DebugMessage("Bad user/password");
-                break;
-            case 5:
-                Debugger::DebugMessage("Not authenticated"); 
-                break;
-            case 6:
-                Debugger::DebugMessage("Failed to subscribe");
-                break;
-            default:
-                String message = "Couldn't connect to server, code: ";
-                char buffer[20];
-                message += itoa(result, buffer, 10);
-                Debugger::DebugMessage(message);
-                break;
-        }
-        Debugger::DebugMessage("Retrying MQTT connection in 5 seconds...");
-        mqtt.disconnect();
-        delay(5000);
-    }
-    Debugger::DebugMessage("MQTT Connected!");
-} 
-
-//
-//  Check if the data was sent to Adafruit IO OK
-//
-void CheckMQTTLoggingResult(bool result, String readingType)
-{
-    if (result)
-    {
-        Debugger::DebugMessage("MQTT Success: " + readingType);
-    }
-    else
-    {
-        Debugger::DebugMessage("MQTT Failed: " + readingType);
-    }
 }
 
 //
@@ -328,16 +216,19 @@ void SetAlarm(DS3231 *rtc, uint8_t period)
     delete(dateTime);
 }
 
+//
+//  Take sensor readings an push the data to the Internet.
+//
 void ReadAndPublishData()
 {
-    char _buffer[20];
-    String _message;
+    char buffer[20];
+    String message;
 
     digitalWrite(PIN_ONBOARD_LED, HIGH);
-    _message = "Reading sensor data (";
-    _message += itoa(_readingNumber, _buffer, 10);
-    _message += ")";
-    Debugger::DebugMessage(_message);
+    message = "Reading sensor data (";
+    message += itoa(_readingNumber, buffer, 10);
+    message += ")";
+    Debugger::DebugMessage(message);
     _sensors->ReadAllSensors();
     Debugger::LogLuminosityData(_sensors->GetLuminosityReading());
     Debugger::LogTemperatureHumidityAndPressureData(_sensors->GetAirTemperature(), _sensors->GetHumidity(), _sensors->GetAirPressure());
@@ -345,30 +236,19 @@ void ReadAndPublishData()
     //Debugger::LogUltravioletData(_sensors->GetUltravioletLightStrength());
     Debugger::LogRainfall(0, _pluviometerCountToday * 0.2794);
     //
-    _message = "Wind speed pulse count: ";
-    _message += itoa(_lastFiveSecondWindSpeedCount, _buffer, 10);
-    Debugger::DebugMessage(_message);
+    message = "Wind speed pulse count: ";
+    message += itoa(_lastFiveSecondWindSpeedCount, buffer, 10);
+    Debugger::DebugMessage(message);
     //
-    _message = "Wind speed: ";
-    _message += Debugger::FloatToAscii(_buffer, (_lastFiveSecondWindSpeedCount * 1.492) / 5, 2);
-    _message += "mph";
-    Debugger::DebugMessage(_message);
+    message = "Wind speed: ";
+    message += Debugger::FloatToAscii(buffer, (_lastFiveSecondWindSpeedCount * 1.492) / 5, 2);
+    message += "mph";
+    Debugger::DebugMessage(message);
     //
     //  Now post to the Internet.
     //
     Debugger::DebugMessage("Posting to Internet.");
     PostDataToPhant();
-    //MQTT_connect();
-    //CheckMQTTLoggingResult(_airPressureFeed.publish(_sensors->GetAirPressure() / 100), "Air Pressure");
-    //CheckMQTTLoggingResult(_airTemperatureFeed.publish(_sensors->GetAirTemperature()), "Air Temperature");
-    //CheckMQTTLoggingResult(_daylightFeed.publish(_sensors->GetLuminosityReading()), "Daylight");
-    //CheckMQTTLoggingResult(_groundTemperatureFeed.publish(_sensors->GetGroundTemperatureReading()), "Ground Temperature");
-    //CheckMQTTLoggingResult(_humidityFeed.publish(_sensors->GetHumidity()), "Humidity");
-    //CheckMQTTLoggingResult(_rainFallFeed.publish(_sensors->GetTotalRainfallToday()), "Rainfall");
-    //CheckMQTTLoggingResult(_windSpeedFeed.publish(_sensors->GetWindSpeed()), "Wind Speed");
-    //CheckMQTTLoggingResult(_windDirectionFeed.publish(_sensors->GetWindDirectionAsString()), "Wind Direction");
-    //CheckMQTTLoggingResult(_uvLightFeed.publish(_sensors->GetUltravioletLightStrength()), "Ultraviolet Light");
-    //Debugger::DebugMessage("Posting complete'");
     digitalWrite(PIN_ONBOARD_LED, LOW);
 }
 
@@ -424,7 +304,7 @@ void PluviometerInterruptHandler()
 //
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println();
     Serial.println();
     Wire.begin();
